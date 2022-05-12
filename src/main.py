11c679f426b68b41e6e39836cc75351463c9ec9a
Илья Дубенский <Ilia@MacBook-Ila.local>
2022-05-12 18:12:53 +0300

    gifs

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 # RandomPetBot/src/main.py
-import os
-from dotenv import load_dotenv
-import giphy_client
 import logging
-import requests
+import os
 import random
+
+import requests
+from dotenv import load_dotenv
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Updater
 
@@ -15,25 +15,52 @@ logging.basicConfig(
 )
 
 secret_token = os.getenv('TG_TOKEN')
+secret_token_giphy = os.getenv('GIPHY_TOKEN')
 updater = Updater(token=secret_token, use_context=True)
+
+rating = 'pg-13'
+cat_tag = 'cat'
+dog_tag = 'dog'
 URL_cats = 'https://api.thecatapi.com/v1/images/search'
 URL_dogs = 'https://api.thedogapi.com/v1/images/search'
-# secret_token_giphy = os.getenv('GIPHY_TOKEN')
+URL_cats_gifs = (
+    'https://api.giphy.com/v1/gifs/random?'
+    f'api_key={secret_token_giphy}&tag={cat_tag}&rating={rating}'
+)
+URL_dogs_gifs = (
+    'https://api.giphy.com/v1/gifs/random?'
+    f'api_key={secret_token_giphy}&tag={dog_tag}&rating={rating}'
+)
 
 
-# def get_random_cat_gif(api_key):
-#     instanse = giphy_client.DefaultApi()
-#     response = instanse.gifs_random_get(api_key, tag='cat')
-#     print(response)
-#     random_gif = response.data.url
-#     return random_gif
+def get_random_cat_gif():
+    response = requests.get(URL_cats_gifs)
+    response = response.json()
+    random_gif = response['data']['images']['downsized']['url']
+    print(random_gif)
+    return random_gif
 
 
-# def new_cat_gif(update, context):
-#     context.bot.send_animation(
-#         chat_id=update.effective_chat.id,
-#         animation=get_random_cat_gif(secret_token_giphy)
-#     )
+def new_cat_gif(update, context):
+    context.bot.send_animation(
+        chat_id=update.effective_chat.id,
+        animation=get_random_cat_gif()
+    )
+
+
+def get_random_dog_gif():
+    response = requests.get(URL_dogs_gifs)
+    response = response.json()
+    random_gif = response['data']['images']['downsized']['url']
+    print(random_gif)
+    return random_gif
+
+
+def new_dog_gif(update, context):
+    context.bot.send_animation(
+        chat_id=update.effective_chat.id,
+        animation=get_random_dog_gif()
+    )
 
 
 def get_random_cat_image():
@@ -72,36 +99,71 @@ def new_dog_image(update, context):
 def wake_up(update, context):
     chat_id = update.effective_chat.id
     name = update.message.chat.first_name
-    # , 'gifs' , ['/new_cat_gif']
-    buttons = ReplyKeyboardMarkup([['/new_cat_image'], ['/new_dog_image']], resize_keyboard=True)
-    
-    pets = ['cats', 'dogs']
-    if random.choice(pets) == 'cats':
+    buttons = ReplyKeyboardMarkup([
+        ['/new_cat_image'],
+        ['/new_dog_image'],
+        ['/new_cat_gif'],
+        ['/new_dog_gif']
+        ],
+        resize_keyboard=True
+    )
+    pets = ['cat_image', 'dog_image', 'cat_gif', 'dog_gif']
+    if random.choice(pets) == 'cat_image':
         context.bot.send_message(
             chat_id=chat_id,
-            text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+            text='Привет, {}. Cмотри, какого котика я тебе нашёл'.format(name),
             reply_markup=buttons
         )
-        context.bot.send_photo(chat_id=chat_id, photo=get_random_cat_image())
-    elif random.choice(pets) == 'dogs':
+        context.bot.send_photo(
+            chat_id=chat_id,
+            photo=get_random_cat_image()
+        )
+    elif random.choice(pets) == 'dog_image':
         context.bot.send_message(
             chat_id=chat_id,
-            text='Привет, {}. Посмотри, какого песика я тебе нашёл'.format(name),
+            text='Привет, {}. Cмотри, какого песика я тебе нашёл'.format(name),
             reply_markup=buttons
         )
-        context.bot.send_photo(chat_id=chat_id, photo=get_random_dog_image())
-    # else:
-    #     context.bot.send_message(
-    #         chat_id=chat_id,
-    #         text='Привет, {}. Посмотри, какую гифку я тебе нашёл'.format(name),
-    #         reply_markup=buttons
-    #     )
-    #     context.bot.send_animation(chat_id=chat_id, animation=get_random_cat_gif(secret_token_giphy))
+        context.bot.send_photo(
+            chat_id=chat_id,
+            photo=get_random_dog_image()
+        )
+    elif random.choice(pets) == 'cat_gif':
+        context.bot.send_message(
+            chat_id=chat_id,
+            text='Привет, {}. Cмотри, какого котика я тебе нашёл'.format(name),
+            reply_markup=buttons
+        )
+        context.bot.send_animation(
+            chat_id=chat_id,
+            animation=get_random_cat_gif()
+        )
+    elif random.choice(pets) == 'dog_gif':
+        context.bot.send_message(
+            chat_id=chat_id,
+            text='Привет, {}. Cмотри, какого песика я тебе нашёл'.format(name),
+            reply_markup=buttons
+        )
+        context.bot.send_animation(
+            chat_id=chat_id,
+            animation=get_random_dog_gif()
+        )
 
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('new_cat_image', new_cat_image))
-updater.dispatcher.add_handler(CommandHandler('new_dog_image', new_dog_image))
-# updater.dispatcher.add_handler(CommandHandler('new_cat_gif', new_cat_gif))
-updater.start_polling()
-updater.idle()
+def main():
+    updater = Updater(token=secret_token)
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(
+        CommandHandler('new_cat_image', new_cat_image)
+    )
+    updater.dispatcher.add_handler(
+        CommandHandler('new_dog_image', new_dog_image)
+    )
+    updater.dispatcher.add_handler(CommandHandler('new_cat_gif', new_cat_gif))
+    updater.dispatcher.add_handler(CommandHandler('new_dog_gif', new_dog_gif))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
